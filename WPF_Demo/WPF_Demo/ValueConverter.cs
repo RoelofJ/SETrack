610d2fbac11327fb238212a76735f420c5e11451
Olaf van der Kruk <olijf@users.noreply.github.com>
2017-12-19 11:24:37 +0100

    Added ValueConverterDemo

---
+++ WPF_Demo/WPF_Demo/ValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPF_Demo
+{
+    public class DoubleToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = System.Convert.ToDouble(value);
+            var p = System.Convert.ToDouble(parameter);
+            
+            if (v > p)
+            {
+                return (object)true;
+            }
+            else
+            {
+                return (object)false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfApp2/WpfApp2/DoubleToBoolConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfApp2
+{
+    class DoubleToBoolConverter : IValueConverter
+
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = System.Convert.ToDouble(value);
+            var p = System.Convert.ToDouble(parameter);
+            if (v > p)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class DoubleToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = System.Convert.ToDouble(value);
+            var p = System.Convert.ToDouble(parameter);
+            if (v > p)
+            {
+                return Color.FromRgb((byte)(v * 10),0,0);
+            }
+            else
+            {
+                return Color.FromRgb(0, 0, (byte)(v * 10));
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
