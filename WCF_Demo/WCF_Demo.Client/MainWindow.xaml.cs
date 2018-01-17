76cb3011a625c662ee542cde5eb5c371b2034bad
Roelof Jansen <roelof.jansen@capgemini.com>
2018-01-17 09:05:10 +0100

    added proxies

---
+++ WCF_Demo/WCF_Demo.Client/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WCF_Demo.Contracts;
+using WCF_Demo.Proxies;
+
+namespace WCF_Demo.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Preliminary_Click(object sender, RoutedEventArgs e)
+        {
+            var validData = FormIsValid();
+            (sender as Button).Background = validData ? Brushes.LightGray : Brushes.Red;
+            if (validData)
+            {
+                var data = ParseForm();
+                var proxy = new CheckAstronautClient();
+                var passed = proxy.DoPreliminaryCheckup(data);
+                PreliminaryResult.Text = passed.ToString();
+                proxy.Close();
+            }
+        }
+
+        private void Final_Click(object sender, RoutedEventArgs e)
+        {
+            var validData = FormIsValid();
+            (sender as Button).Background = validData ? Brushes.LightGray : Brushes.Red;
+            if (validData)
+            {
+                var data = ParseForm();
+                var proxy = new CheckAstronautClient();
+                var passed = proxy.DoFinalCheckup(data);
+                FinalResult.Text = passed.ToString();
+                proxy.Close();
+            }
+        }
+
+        private FlightRosterData ParseForm()
+        {
+            return new FlightRosterData
+            {
+                CaptainName = CaptainName.Text,
+                SecondInCommandName = SecondName.Text,
+                NavigatorName = NavigatorName.Text,
+                EngineerName = EngineerName.Text
+            };
+        }
+
+        private bool FormIsValid()
+        {
+            return 
+                !string.IsNullOrWhiteSpace(CaptainName.Text) &&
+                !string.IsNullOrWhiteSpace(SecondName.Text) &&
+                !string.IsNullOrWhiteSpace(NavigatorName.Text) &&
+                !string.IsNullOrWhiteSpace(EngineerName.Text);
+        }
+    }
+}
---
+++ WCF_Demo/WCF_Demo.Proxies/CheckAstronautClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCF_Demo.Contracts;
+
+namespace WCF_Demo.Proxies
+{
+    public class CheckAstronautClient : ClientBase<ICheckAstronautsService>, ICheckAstronautsService
+    {
+        public bool DoFinalCheckup(FlightRosterData data)
+        {
+            return Channel.DoFinalCheckup(data);
+        }
+
+        public bool DoPreliminaryCheckup(FlightRosterData data)
+        {
+            return Channel.DoPreliminaryCheckup(data);
+        }
+
+        public AstronautData GetAstronaut(string name)
+        {
+            return Channel.GetAstronaut(name);
+        }
+    }
+}
