206971703351fb67165eae57ddfe246646db34b9
Roelof Jansen <roelof.jansen@capgemini.com>
2018-01-23 09:40:22 +0100

    forgot to add MyFaultData datacontract

---
+++ WCF_Demo/WCF_Demo.Contracts/MyFaultData.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace WCF_Demo.Contracts
+{
+    [DataContract]
+    public class MyFaultData
+    {
+        [DataMember]
+        public string Message { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int Index { get; set; }
+    }
+}
